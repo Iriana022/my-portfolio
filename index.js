93f88d207088c2c2a3c07d06dd3b5e8df5c1fc88
Iriana R <dev32.irin@gmail.com>
2024-11-26 09:02:38 +0300

    page effect

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+AOS.init();
+
 const menu_button = document.querySelector(".menu-burger")
 const sidebar = document.querySelector(".side")
 menu_button.addEventListener("click", () => {
     sidebar.classList.toggle("in")
     menu_button.classList.toggle("in")
 })
+
